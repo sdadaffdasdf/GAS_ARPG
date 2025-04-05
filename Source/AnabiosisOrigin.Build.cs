fa38e41b4f14bb9ba9e6b198a14d505db44f710b
Wang <2726334183@qq.com>
2025-04-05 18:11:13 +0800

    Update

--- Source/AnabiosisOrigin.Build.cs
+++ Source/AnabiosisOrigin.Build.cs
@@ -1,33 +1,37 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.Collections.Generic;
 
 public class AnabiosisOrigin : ModuleRules
 {
-	public AnabiosisOrigin(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public AnabiosisOrigin(ReadOnlyTargetRules Target) : base(Target)
+    {
+        bEnforceIWYU = true;
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicIncludePaths.AddRange(new string[] {
-			"AnabiosisOrigin/Public"
-		});
+        PublicIncludePaths.AddRange(new string[] {
+            "AnabiosisOrigin/Public"
+        });
 
-		PrivateIncludePaths.AddRange(new string[] {
-			"AnabiosisOrigin/Private"
-		});
+        PrivateIncludePaths.AddRange(new string[] {
+            "AnabiosisOrigin/Private"
+        });
 
-		PublicDependencyModuleNames.AddRange(new string[] { 
-			"Core", 
-			"CoreUObject", 
-			"Engine", 
-			"InputCore", 
-			"HeadMountedDisplay",
-			"EnhancedInput",
-			"GameplayAbilities",
-			"GameplayTags",
-			"GameplayTasks"
-		});
+        PublicDependencyModuleNames.AddRange(new string[] { 
+            "Core", 
+            "CoreUObject", 
+            "Engine", 
+            "InputCore", 
+            "HeadMountedDisplay",
+            "EnhancedInput",
+            "GameplayAbilities",
+            "GameplayTags",
+            "GameplayTasks"
+        });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-	}
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        bUseUnity = false;  // 禁用Unity构建以更精确地控制哪些文件被编译
+    }
 }
